180cc455f1fc4503b7cc2fe993e101ce110277f1
Alex-byte256 <oleksandr.kozak.2019@gmail.com>
2023-02-19 17:01:59 +0300

    dodaw dodavanya nomeriv

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,42 @@
-export const App = () => {
-  return (
-    <div
-      
-    >
-      React homework template
-    </div>
-  );
+import PhoneBook from './PhoneBook/PhoneBook';
+import { Component } from 'react';
+import { nanoid } from 'nanoid'
+
+class  App extends Component{
+  state = {
+    contacts: [],
+    name: ''
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      contacts: [...this.state.contacts,
+        { name:this.state.name, id: nanoid(10) }
+      ],name: ""});
+  }
+
+  handleOnChange = (e) => {
+    console.log(e.target.value)
+    this.setState( ({
+      name: e.target.value
+    }))
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Phone book</h2>
+        <PhoneBook onSubmit={this.handleSubmit} textValue={this.state.name} onChange={this.handleOnChange}/>
+        <h2>Contacts </h2>
+        <ul>
+          {this.state.contacts.map(el => (
+            <li key={el.id}>{el.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 };
+
+export default App;
